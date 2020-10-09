46f2c495576e94c0b990d89ff4a68a1b15b71ed8
MolokanovAD <55701106+MolokanovAD@users.noreply.github.com>
2020-10-09 13:56:09 +0300

    Update main.cpp

--- Project3/Project3_3/main.cpp
+++ Project3/Project3_3/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+include <iostream>
 #include "Hex.h"
 
 int main() {
@@ -60,7 +60,10 @@ int main() {
 					continue;
 				}
 				a >>= r;
-				std::cout << a << std::endl;
+				if(r < 0)
+					std::cout << "Incorrect input" << std::endl;
+				else
+					std::cout << a << std::endl;
 				break;
 			case 2:
 				std::cout << "Enter number of digits" << std::endl;
@@ -72,7 +75,10 @@ int main() {
 					continue;
 				}
 				a <<= r;
-				std::cout << a << std::endl;
+				if (r < 0)
+					std::cout << "Incorrect input" << std::endl;
+				else
+					std::cout << a << std::endl;
 				break;
 			case 3:
 				std::cout << a << " " << a.Compare(b) << " " << b << std::endl;
