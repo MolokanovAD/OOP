85bbc43af29f9b8ca64587936d0604af4ec646d0
MolokanovAD <55701106+MolokanovAD@users.noreply.github.com>
2020-10-15 22:32:51 +0300

    Update Hex.h

--- Project3/Project3_1/Hex.h
+++ Project3/Project3_1/Hex.h
@@ -2,6 +2,7 @@
 #define HEX_H
 #include <iostream>
 const int len = 32;
+const int N = 10;
 namespace Project3_1 {
 	class Hex {
 	private:
@@ -15,16 +16,19 @@ namespace Project3_1 {
 		char getSign() const{ return number[0]; }
 		int Check() const;
 		Hex& Convert();
+		char Compare(const Hex&);
 		const Hex Add(const Hex&);
 		const Hex Decrease(const Hex&);
 		Hex& Move_l(int);
 		Hex& Move_r(int);
-		char Compare(const Hex&);
 		std::istream& Input(std::istream&);
 		std::ostream& Print(std::ostream&);
 	};
 	char upper(const char);
 	char HexToChar(int);
 	int CharToHex(char);
+
+	void dialog(Hex&, Hex&);
+	void out(Hex&, Hex&, Hex&, const char*);
 }
 #endif
