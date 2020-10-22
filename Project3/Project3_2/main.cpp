266d84e84a4c594b5a3e3500e70b3e9415aa1e55
MolokanovAD <55701106+MolokanovAD@users.noreply.github.com>
2020-10-22 22:52:24 +0300

    Update main.cpp

--- Project3/Project3_2/main.cpp
+++ Project3/Project3_2/main.cpp
@@ -1,8 +1,79 @@
 #include <iostream>
 #include "Hex.h"
 
+void dialog(H16_2::Hex& a, H16_2::Hex& b) {
+	int k = 0;
+	while (k != 5) {
+		std::cout << "1) Move first operand to the right" << std::endl << "2) Move first operand to the left" << std::endl << "3) Compare operands" << std::endl << "4) Check parity of first operand" << std::endl << "5) Exit" << std::endl;
+		try {
+			std::cin >> k;
+		}
+		catch (std::exception & b) {
+			std::cout << b.what() << std::endl;
+		}
+		switch (k) {
+		case 1:
+			int r;
+			std::cout << "Enter number of digits" << std::endl;
+			try {
+				std::cin >> r;
+			}
+			catch (std::exception & b) {
+				std::cout << b.what() << std::endl;
+				continue;
+			}
+			try {
+				a >>= r;
+			}
+			catch (std::exception & b) {
+				std::cout << b.what() << std::endl;
+				continue;
+			}
+			/*if (r < 0)
+				std::cout << "Invalid input" << std::endl;
+			else*/
+			std::cout << a << std::endl;
+			break;
+		case 2:
+			std::cout << "Enter number of digits" << std::endl;
+			try {
+				std::cin >> r;
+			}
+			catch (std::exception & b) {
+				std::cout << b.what() << std::endl;
+				continue;
+			}
+			try {
+				a <<= r;
+			}
+			catch (std::exception & b) {
+				std::cout << b.what() << std::endl;
+				continue;
+			}
+			/*if (r < 0)
+				std::cout << "Invalid input" << std::endl;
+			else*/
+			std::cout << a << std::endl;
+			break;
+		case 3:
+			std::cout << a << " " << a.Compare(b) << " " << b << std::endl;
+			break;
+		case 4:
+			if (!a.Check())
+				std::cout << a << " is an even number" << std::endl;
+			else
+				std::cout << a << " isn't an even number" << std::endl;
+			break;
+		case 5:
+			continue;
+		default:
+			std::cout << "You are wrong, repeat please" << std::endl;
+		}
+	}
+}
 int main() {
-	Project3_2::Hex a, b;
+
+	H16_2::Hex a, b;
 	int flag = 1;
 	while (flag) {
 		std::cout << "Enter first hexadecimal value" << std::endl;
@@ -20,7 +91,7 @@ int main() {
 			continue;
 		}
 		try {
-			Project3_2::Hex c = a + b;
+			H16_2::Hex c = a + b;
 			if (b.getSign() == 'F')
 				std::cout << a << " + (" << b << ") = " << c << std::endl;
 			else
@@ -30,7 +101,7 @@ int main() {
 			std::cout << b.what() << std::endl;
 		}
 		try {
-			Project3_2::Hex c = a - b;
+			H16_2::Hex c = a - b;
 			if (b.getSign() == 'F')
 				std::cout << a << " - (" << b << ") = " << c << std::endl;
 			else
